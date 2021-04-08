23c1591db686239a790850cd3ff77edb3210a673
Michael Stevens <michael-stevens@Michael-Stevens-MacBook-Pro.local>
2021-04-08 14:56:11 -0600

    New updates

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -12,7 +12,7 @@ function About(){
     return(
         <Container>
             <Subtitle>About</Subtitle>
-            <Display>I am currently a Sr. Product Designer at <a href="http://www.pluralsight.com/" target="_blank" rel="noopener noreferrer">Pluralsight</a>, a freelance from <span>time-to-time</span> and an Adjunct Professor.</Display>
+            <Display>I am currently a Sr. Product Designer at <a href="http://www.pluralsight.com/" target="_blank" rel="noopener noreferrer">Pluralsight</a>, a Freelancer from <span>time-to-time</span> and an Adjunct Professor.</Display>
             <Heading theme="dark">
                 Check the list below to see some of the skills I've obtained throughout
                 the years. You can always <a href="mailto:mich8060@hotmail.com" target="_blank" rel="noopener noreferrer">email me</a> to learn more.
@@ -21,11 +21,12 @@ function About(){
                 <div className="list--column">
                     <div className="list--title">Education</div>
                     <Role title="Adjunct Professor" timeline="Weber State University – Aug 2020 to Present" />
-                    <Role title="Senior Product Designer" timeline="Pluralsight.com – July 2016 to Present" />
+                    <Role title="Sr. Product Designer" timeline="Pluralsight.com – July 2016 to Present" />
                     <Role title="Principal Designer" timeline="Backcountry.com – January 2015 to July 2016" />
-                    <Role title="Senior User Experience Design Manager" timeline="Backcountry.com – November 2014 to January 2015" />
-                    <Role title="Senior User Experience Designer" timeline="Backcountry.com – June 2012 - November 2014" />
-                    <Role title="Senior User Experience Designer" timeline="Skullcandy – July 2011 - June 2012" />
+                    <Role title="Sr. User Experience Design Manager" timeline="Backcountry.com – November 2014 to January 2015" />
+                    <Role title="Sr. User Experience Designer" timeline="Backcountry.com – June 2012 - November 2014" />
+                    <Role title="Product Manager" timeline="Backcountry.com – June 2012 - January 2013" />
+                    <Role title="Sr. User Experience Designer" timeline="Skullcandy – July 2011 - June 2012" />
                     <Role title="User Experience Designer" timeline="Backcountry.com – April 2008 - July 2011" />
                     <Role title="Rapid Prototype Developer" timeline="Backcountry.com – August 2007 - April 2008" />
                     <Role title="Graphic Designer" timeline="Backcountry.com – July 2006 - August 2007" />
@@ -36,11 +37,11 @@ function About(){
                     <Role title="Weber State University" timeline="BFA, Visual Communications" />
                     <Role title="Ogden Applied Technology College" timeline="Technical Associates Degree, Multimedia &amp; Web Development" />
                     <div className="list--title">Skills</div>
-                    <Role title="Design" timeline="Layout Hierarchy, Typography, Color Theory, Branding, Adobe Creative Suite, Creative Thinking" />
+                    <Role title="Design" timeline="Layout Hierarchy, Typography, Color Theory, Branding, Adobe Creative Suite, Creative Thinking and Design System Creation." />
                     <Role title="Technology" timeline="HTML5, CSS, SASS, jQuery, PHP, SQL, JavaScript, Node, React.js, Vue.js" />
-                    <Role title="Research" timeline="Usability Testing, Survey Composition, Analytics Technologies, User Interviews, Persona Development" />
-                    <Role title="Product Management" timeline="Analytics, OKR creation &amp; management, Executive &amp; Stakeholder communications &amp; presentation" />
-                    <Role title="Servant Leadership &amp; Management" timeline="1:1 Communication, Career Growth, Active Listening, Problem Solving, Conflict Management, Interpersonal Relationship Development and Mentorship." />
+                    <Role title="Research" timeline="Usability Testing, Survey Composition, Analytics Technologies, User Interviews, Persona Development." />
+                    <Role title="Product Management" timeline="Analytics, OKR creation &amp; management, Executive &amp; Stakeholder communications &amp; presentation, Public Speaking and Strategy Development." />
+                    <Role title="Servant Leadership &amp; Management" timeline="1:1 Communication, Career Growth, active listening, problem solving, conflict management, interpersonal relationship development and mentorship." />
                 </div>
             </List>
         </Container>
