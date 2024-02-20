fe63081fd76a3a7be3e25deda9135cb989023bf9
Michael Stevens <mich8060@hotmail.com>
2024-02-20 08:37:33 -0700

    Small Tweaks

--- src/components/work/index.jsx
+++ src/components/work/index.jsx
@@ -7,7 +7,7 @@ function Work({children}){
                 <div className="work--column">
                     <small>Experience</small>
                     <ul>
-                        <li><strong>Head of Product & Design</strong> Project Broadcast – April 2022 to Present</li>
+                        <li><strong>Principal Product Designer</strong> Project Broadcast – April 2022 to Present</li>
                         <li><strong>Principal Product Designer</strong> Pluralsight.com – June 2021 to April 2022</li>
                         <li><strong>Adjunct Professor</strong> Weber State University – Aug 2020 to June 2021</li>
                         <li><strong>Sr. Product Designer</strong> Pluralsight.com – July 2016 to June 2021</li>
@@ -29,11 +29,12 @@ function Work({children}){
                     </ul>
                     <small>Skills</small>
                     <ul>
-                        <li><strong>Design</strong> Figma, Figma Prototyping, Layout Hierarchy, Typography, Color Theory, Branding, Adobe Creative Suite, Creative Thinking, Brand Language Development, and Design System Creation & Management.</li>
-                        <li><strong>Technology</strong> HTML5, CSS, SASS, jQuery, PHP, SQL, JavaScript, Node, React.js, Vue.js, Next.js, Express</li>
+                        <li><strong>User Experience Design</strong> Figma, Figma Prototyping, Responsive Wireframing, Prototyping, User Flows, Journey Mapping, Interaction Design, Design System Creation & Management.</li>
+                        <li><strong>Visual Design</strong> Layout Hierarchy, Typography, Color Theory, Branding, Adobe Creative Suite, Brand Language Development, Illustration, Iconography, Visual Storytelling.</li>
+                        <li><strong>Technology</strong> HTML5, CSS, SASS, jQuery, PHP, SQL, JavaScript (Node, React.js, Vue.js, Next.js), Express.</li>
                         <li><strong>Research</strong> Usability Testing, Survey Composition, Analytics Technologies, User Interviews, Persona Development.</li>
-                        <li><strong>Product Management</strong> Analytics, OKR creation & management, Executive & Stakeholder communications & presentation, Public Speaking and Strategy Development.</li>
-                        <li><strong>Servant Leadership & Management</strong> 1:1 Communication, Career Growth, active listening, problem solving, conflict management, interpersonal relationship development and mentorship.</li>
+                        <li><strong>Product Management</strong> Analytics, OKR Creation & Management, Executive & Stakeholder Communications & Presentation, Agile Methodologies, Public Speaking, Strategy Development.</li>
+                        <li><strong>Servant Leadership & Management</strong> 1:1 Communication, Presentation Skills, Career Growth, Active Listening, Problem Solving, Conflict Management, Interpersonal Relationship Development, Mentorship.</li>
                     </ul>
                 </div>
             </div>
